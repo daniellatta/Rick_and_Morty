5bdfc0f0630a605f9b119d93709b36c99965f685
daniellatta <danylatta1433@gmail.com>
2023-03-31 16:22:07 -0500

    formatted

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,21 +1,25 @@
 import Card from "./Card";
 
-export default function Cards(props) {
+export default function Cards({ characters }) {
   return (
     <div>
-      {props.characters.map((character) => {
-        return(
-        <Card
-          key={character.id}
-          name= {character.name}
-          status={character.status}
-          species={character.species}
-          gender={character.gender}
-          origin={character.origin}
-          image={character.image}
-          onClose={() => window.alert('Emulamos que se cierra la card de ' + character.name)}
-        />
-        )
+      {characters.map((character) => {
+        return (
+          <Card
+            key={character.id}
+            name={character.name}
+            status={character.status}
+            species={character.species}
+            gender={character.gender}
+            origin={character.origin}
+            image={character.image}
+            onClose={() =>
+              window.alert(
+                "Emulamos que se cierra la card de " + character.name
+              )
+            }
+          />
+        );
       })}
     </div>
   );
